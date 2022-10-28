9af25d09fc105942d7ec0b924c3376a40a15099b
Yoo-sumi <hugbee@naver.com>
2022-10-29 00:18:20 +0900

    feat: 백준 18500 해결

---
+++ CodingTest/src/main/kotlin/Solution15.kt
@@ -0,0 +1,110 @@
+// https://www.acmicpc.net/problem/18500
+
+class Solution15(val r: Int, val c: Int) {
+    val graph = Array(r) { mutableListOf<Char>() }
+    var temp = Array(r) { Array(c) { 0 } }
+    val dx = listOf(-1,1,0,0)
+    val dy = listOf(0,0,-1,1)
+
+    fun bfs(x: Int, y: Int) {
+        temp[x][y] = 1
+        val q = ArrayDeque<Points>()
+        q.add(Points(x, y))
+        while (q.isNotEmpty()) {
+            val now = q.removeFirst()
+            repeat(4) {
+                val nx = now.x + dx[it]
+                val ny = now.y + dy[it]
+                if (nx < 0 || nx >= r || ny < 0 || ny >= c) return@repeat
+                if (temp[nx][ny] == 0 && graph[nx][ny] == 'x') {
+                    q.add(Points(nx, ny))
+                    temp[nx][ny] = 1
+                }
+            }
+        }
+    }
+
+    fun removeMinerals(stick: Int, direction: Boolean) {
+        if (direction) {
+            for (i in 0 until c) {
+                if (graph[stick][i] == 'x') {
+                    graph[stick][i] = '.'
+                    break
+                }
+            }
+        } else {
+            for (i in c-1 downTo 0) {
+                if (graph[stick][i] == 'x') {
+                    graph[stick][i] = '.'
+                    break
+                }
+            }
+        }
+    }
+
+    fun solution() {
+        repeat(r) {
+            graph[it] = readln().toMutableList()
+        }
+        val n = readln().toInt()
+        val sticks = readln().split(" ").map { r - it.toInt() }
+
+        var type = 1
+        sticks.forEach { stick ->
+            removeMinerals(stick, type++ % 2 == 1)
+            temp = Array(r) { Array(c) { 0 } }
+            for (i in 0 until c) {
+                if (temp[r-1][i] == 0 && graph[r-1][i] == 'x') {
+                    bfs(r-1, i)
+                }
+            }
+            val store = mutableListOf<Points>()
+            for (i in 0 until r) {
+                for (j in 0 until c) {
+                    if (temp[i][j] != 1 && graph[i][j] == 'x') {
+                        store.add(Points(i, j))
+                    }
+                }
+            }
+            if (store.isNotEmpty()) {
+                var flag = true
+                var cc = 0
+                while (flag) {
+                    for (s in 0 until store.size) {
+                        if (store[s].x + cc == r-1) {
+                            flag = false
+                            break
+                        }
+                        if (temp[store[s].x + cc + 1][store[s].y] == 1) {
+                            flag = false
+                            break
+                        }
+                    }
+                    if (flag) {
+                        cc++
+                    }
+                }
+                store.sortBy { -it.x }
+                store.forEach {
+                    graph[it.x][it.y] = '.'
+                    graph[it.x + cc][it.y] = 'x'
+                }
+            }
+        }
+        graph.forEach {
+            it.forEach {
+                print("$it")
+            }
+            println()
+        }
+    }
+}
+
+data class Points(
+    val x: Int,
+    val y: Int
+)
+fun main() {
+    val (r, c) = readln().split(" ").map { it.toInt() }
+    Solution15(r, c).solution()
+}
